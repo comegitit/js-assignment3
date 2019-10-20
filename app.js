7d3a7c83fe7f3cdf15e120bf3352b4b9632de9ea
comegitit <aggarwal.roy@gmail.com>
2019-10-19 23:16:14 -0600

    code cleanup
    comments & formatting

--- app.js
+++ app.js
@@ -1,49 +1,53 @@
 window.addEventListener('load', () => {
-  let lat ='instantiation'
+
+  let lat = 'instantiation'
   let long = 'instantiation'
   const tempInDegrees = document.querySelector('.temp-in-degrees')
   const SummaryDescription = document.querySelector('.summary-description')
   const locationTimezone = document.querySelector('.location-timezone')
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      lat = position.coords.latitude
-      long = position.coords.longitude
+  //use built-in javascript functionality to fetch lat & long
+  navigator.geolocation.getCurrentPosition(position => {
+    lat = position.coords.latitude
+    long = position.coords.longitude
 
-      //proxy workaround for browser CORS policy restrictions
-      const proxy = "https://cors-anywhere.herokuapp.com/"
-      //also added query string to return units (celsius) relevant to Canadian users
-      const api = `${proxy}https://api.darksky.net/forecast/22dc744c9e56c0455e206506c8a258f4/${lat},${long}/?units=si`
+    //proxy workaround for browser CORS policy restrictions
+    const proxy = "https://cors-anywhere.herokuapp.com/"
+    //using 'Dark Sky' API with query string for metric units (celsius)
+    const api = `${proxy}https://api.darksky.net/forecast/22dc744c9e56c0455e206506c8a258f4/${lat},${long}/?units=si`
 
+    //fetch API data & convert to JSON
+    fetch(api)
+      .then(response => {
+        return response.json()
+      })
 
-      fetch(api)
-        .then(response => {
-          return response.json()
-        })
-        .then(data => {
-          //console.log(data)
-          const {temperature, summary, icon}=data.currently
+      .then(data => {
+        //console.log(data). Leave here for future testing purposes
 
-          //Set DOM elements from the API
-          tempInDegrees.textContent = Math.round(temperature)
-          SummaryDescription.textContent = summary
-          locationTimezone.textContent = data.timezone
+        const {temperature, summary,icon
+        } = data.currently
 
-          //Set the Skycon icon
-          setIcons(icon, document.querySelector('.icon'))
-        })
+        tempInDegrees.textContent = Math.round(temperature)
+        SummaryDescription.textContent = summary
+        locationTimezone.textContent = data.timezone
+
+        //Set the Skycon icon
+        setIcons(icon, document.querySelector('.icon'))
+      })
+  })
+
+  function setIcons(icon, iconId) {
+    const skycons = new Skycons({
+      color: 'white'
     })
 
-  }
-
-  function setIcons(icon, iconID) {
-    const skycons = new Skycons({color:'white'})
-
     //replace dashes with underscores, convert to upper case
-    const currentIcon = icon.replace(/-/g,"_").toUpperCase()
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase()
+    //animate 
     skycons.play()
 
-    return skycons.set(iconID, Skycons[currentIcon])
+    return skycons.set(iconId, Skycons[currentIcon])
   }
 
 })
